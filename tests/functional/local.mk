3eeee44c38ea4e266a8be1e220222f876bf328df
Ondrej Telka <me@ondt.dev>
2024-01-29 15:16:11 +0100

    Disable failing test

--- tests/functional/local.mk
+++ tests/functional/local.mk
@@ -66,7 +66,6 @@ nix_tests = \
   build-remote-trustless-should-pass-1.sh \
   build-remote-trustless-should-pass-2.sh \
   build-remote-trustless-should-pass-3.sh \
-  build-remote-trustless-should-fail-0.sh \
   nar-access.sh \
   pure-eval.sh \
   eval.sh \
